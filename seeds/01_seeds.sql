07d74f80f8aa9ba0c384bd3eb66dd14ac213ebd9
patelheta <patelheta385@gmail.com>
2022-11-16 14:59:50 -0800

    Insert data in each tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users
+  (name, email, password)
+VALUES
+  ('Armand Hilll', 'lera_hahn@dickens.org', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+  ('Stephanie Wolff', 'darius.homenick@tod.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+  ('Stan Miller', 'mcdermott.maxie@schoen.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
