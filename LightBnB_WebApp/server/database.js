7d4f8aee473cb89a001198fd4e1443808541d874
patelheta <patelheta385@gmail.com>
2022-11-17 21:15:20 -0800

    Refactoring getAllReservation function using select query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -96,7 +96,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let queryString = `SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+  let values = [guest_id, limit];
+  return pool.query(queryString, values)
+    .then((result) => {
+      console.log(result);
+      if (result.rowCount > 0) {
+        return result.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
 };
 exports.getAllReservations = getAllReservations;
 
